10929c5130850548d7caa63069de4e55972f5570
fengcheng <lufengc@qq.com>
2017-04-20 19:56:25 +0800

    add swagger2

--- boot-web/src/main/java/com/dsjk/boot/web/user/UserController.java
+++ boot-web/src/main/java/com/dsjk/boot/web/user/UserController.java
@@ -5,6 +5,8 @@ import com.dsjk.boot.common.base.Global;
 import com.dsjk.boot.common.bean.user.SysUser;
 import com.dsjk.boot.common.service.user.UserService;
 import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +30,7 @@ public class UserController {
         return userService.getUserBySql(id);
     }
 
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public SysUser get(@PathVariable String id) {
         return userService.get(id);
@@ -38,6 +41,7 @@ public class UserController {
         return userService.get(user);
     }
 
+    @ApiOperation(value="获取用户列表", notes="")
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public List<SysUser> getList(SysUser user) {
         return userService.getList(user);
@@ -48,11 +52,15 @@ public class UserController {
         return userService.getPage(user);
     }
 
+    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "SysUser")
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public void save(SysUser user) {
         userService.save(user);
     }
 
+    @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
     public void delete(SysUser user) {
         userService.delete(user);
