b74addfc2ddc667f0c6cca2c932be3dc8ed0551c
fengcheng <lufengc@qq.com>
2017-07-12 10:54:14 +0800

    优化接口格式

--- boot-common/src/main/java/com/dsjk/boot/common/utils/JSONUtils.java
+++ boot-common/src/main/java/com/dsjk/boot/common/utils/JSONUtils.java
@@ -2,7 +2,6 @@ package com.dsjk.boot.common.utils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.PropertyFilter;
-import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
 /**
@@ -14,8 +13,6 @@ public class JSONUtils {
     public static Object JSONFilter(Object object) {
         PropertyFilter filter = (ob, key, value) -> !"remarks,createBy,createDate,updateBy,updateDate,delFlag".contains(key);
         String s = JSON.toJSONString(object, filter, SerializerFeature.WriteMapNullValue);
-        SerializeConfig config = SerializeConfig.getGlobalInstance();
-        config.addFilter(object.getClass(), filter);
         return JSON.parse(s);
     }
 }
