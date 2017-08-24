13d5ef1959ae88f56741d148f9b1a0d8665c07ef
cc <lufengc@163.com>
2017-08-24 21:32:22 +0800

    feat: 增加权限角色控制

--- boot-web/src/main/java/com/dsjk/boot/web/security/UserUtils.java
+++ boot-web/src/main/java/com/dsjk/boot/web/security/UserUtils.java
@@ -2,14 +2,18 @@ package com.dsjk.boot.web.security;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.dsjk.boot.common.base.Global;
+import com.dsjk.boot.common.bean.user.Menu;
+import com.dsjk.boot.common.bean.user.Role;
 import com.dsjk.boot.common.bean.user.User;
 import com.dsjk.boot.common.service.user.UserService;
 import com.dsjk.boot.web.config.SpringContextHolder;
+import com.google.common.collect.Lists;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -46,7 +50,23 @@ public class UserUtils {
      * @return 用户信息
      */
     public static User getUserByLoginName(String loginName) {
-        return userService.getUserByLoginName(loginName);
+        User user = userService.getUserByLoginName(loginName);
+        if (user == null) {
+            return null;
+        }
+        user.setRoleList(userService.getRoleByUserId(user.getId()));
+        return user;
+    }
+
+    /**
+     * 获取当前登录者ID
+     *
+     * @return 当前用户id
+     */
+    public static String getUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        JwtUser jwtUser = (JwtUser) authentication.getPrincipal();
+        return jwtUser.getId();
     }
 
     /**
@@ -55,9 +75,29 @@ public class UserUtils {
      * @return 用户信息
      */
     public static User getUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        JwtUser jwtUser = (JwtUser) authentication.getPrincipal();
-        return userService.get(jwtUser.getId());
+        return userService.get(getUserId());
+    }
+
+    /**
+     * 获取当前用户角色列表
+     *
+     * @return 角色列表
+     */
+    public static List<Role> getRoleList() {
+        return userService.getRoleByUserId(getUserId());
+    }
+
+    /**
+     * 获取当前用户授权菜单
+     *
+     * @return 权限菜单列表
+     */
+    public static List<Menu> getMenuList() {
+        List<Menu> menuList = userService.getMenuByUserId(getUserId());
+        List<Menu> list = Lists.newArrayList();
+        Menu.sort(menuList);
+        Menu.sortList(list, menuList, Menu.getRootId(), true);
+        return list;
     }
 
 }
