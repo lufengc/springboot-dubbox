2c7c1ce93f129f560c4594d824e542ed0f8c5b6c
fengcheng <lufengcheng@bdfint.com>
2018-12-04 11:17:54 +0800

    reformat

--- boot-common/src/main/java/com/cheng/boot/common/utils/Reflections.java
+++ boot-common/src/main/java/com/cheng/boot/common/utils/Reflections.java
@@ -1,5 +1,5 @@
 /*
- * Copyright &copy; <a href="http://www.zsteel.cc">zsteel</a> All rights reserved.
+ * Copyright &copy; <a href="http://www.lufengc.cc">lufengc</a> All rights reserved.
  */
 
 package com.cheng.boot.common.utils;
